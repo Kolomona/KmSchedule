ecd8702e0b9506fcf9ec1583d9f2b4da69333d15
Kolomona <kolomona@kolomona.com>
2020-03-22 17:00:52 -0700

    Changed default string length in to solve SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Solves SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;
+        Builder::defaultStringLength(191); // Update defaultStringLength
+
     }
 }
